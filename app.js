cab883068e3e91a3f83df6c6fb0f3343c7fd00ff
murza-08 <ergeshovmurza.08@gmail.com>
2024-11-29 23:50:26 +0600

    5 секшн буттум

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 const data = [
     {
-        img:"./img/5 section/Rectangle 172.svg",
+        img:"./img/5section/Group3.svg",
         name: "Сердце",
-       
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: "./img/5section/Group3(2).svg"
+    },
+    {
+        img:"./img/5section/Group3.svg",
+        name: "Сердце",
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: ""
+    },
+    {
+        img:"./img/5section/Group3.svg",
+        name: "Сердце",
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: "./img/5section/Group3(2).svg"
+    },
+    {
+        img:"./img/5section/Group3.svg",
+        name: "Сердце",
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: "./img/5section/Group3(2).svg"
+    },
+    {
+        img:"./img/5section/Group3.svg",
+        name: "Сердце",
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: "./img/5section/Group3(2).svg"
+    },
+    {
+        img:"./img/5section/Group3.svg",
+        name: "Сердце",
+        btn: "В корзину",
+        p: "24 штуки в коробке в виде сердца. <br> Ассорти из 6 вкусов ",
+        h2: "2800 руб",
+        img1: "./img/5section/Group3(2).svg"
     },
 ];
 const root = document.getElementById("root");
@@ -10,10 +53,13 @@ data.forEach((item) => {
     const datalist = document.createElement("div");
     datalist.innerHTML = `
     
-    <div>
-    <img  src=${item.img}>
-    <h1> ${item.name}</h1>
-   
+    <div class=" border-4 w-[370px] h-[459px] shadow-2xl">
+    <img  src=${item.img} class="transition-transform duration-500 hover:scale-110">
+    <h1 class=" text-[18px] font-semibold ml-[21px] "> ${item.name}</h1>
+    <p class="text-[14px] text-[#292929] font-normal ml-[21px] "> ${item.p}</P>
+    <h2 Class=" ml-[20px] text-[#E7426A] text-[18px] font-semibold  "> ${item.h2}</h2>
+     <button class=" w-[173px] h-[49px] border-2 ml-[190px] mt-[12px]"> ${item.btn}</button>
+     <img  src=${item.img1} class="mt-[-35px] ml-[210px]">
     </div>
     `;
     root.appendChild(datalist)
